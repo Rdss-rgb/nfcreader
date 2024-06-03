4b951d5fbf1c287a7b00879633fbe53dc3528f09
Rdss-rgb <drendarjosh@gmail.com>
2024-06-03 16:38:49 +0800

    remove alert

--- assets/js/app.js
+++ assets/js/app.js
@@ -36,7 +36,6 @@ scanButton.addEventListener("click", async () => {
      else{
       document.getElementById('output-nfc').innerText=`Serial Number: ${serialNumber}  `+`\n`+ `Byte length:`+mess;
       console.log(message.records[0]['data']['byteLength']) 
-      console.log(message.records[0]['data']['byteLength'][[[Uint8Array]]])
 
      }
      console.log(message.records)
