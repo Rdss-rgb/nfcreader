85af77b97f3868ceebc23a52ab2bf83b7882646a
Rdss-rgb <drendarjosh@gmail.com>
2024-06-08 19:53:35 +0800

    space the write commands by a second

--- assets/js/app.js
+++ assets/js/app.js
@@ -104,7 +104,7 @@ scanButton.addEventListener("click", async () => {
           // }
         }
         console.log(byteMsg);
-        setTimeout(sendNFCData(index, ndef, byteMsg), (index/payloadlength) * 2000);
+        setTimeout(sendNFCData, (index/payloadlength) * 2000, index, ndef, byteMsg);
         
         
 
